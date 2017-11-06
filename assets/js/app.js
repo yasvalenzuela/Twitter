c05e9b4024205df2c9a2598b0cc815d60f5c3002
yasvalenzuela <yasna.valenzuela91@gmail.com>
2017-11-06 20:41:48 -0300

    twitter version 0.0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,28 @@
+//para que el boton inicie deshabilitado
+document.getElementById('boton').disabled=true;
+
+//para obtener el valor del texto ingresado
+var comentario = document.getElementById('twit');
+comentario.addEventListener('keyup', function(){
+	//crear una variable en donde quede la longitud del comentario que se ingresa a textarea
+	var longitudComentario = comentario.value.length;
+	var contador = longitudComentario;
+	var maxCharacter = 140;
+	//cuenta en forma regresiva
+	maxCharacter = 140 - contador;
+	//para que aparezca en el html
+	document.getElementById('contCharacter').innerHTML=maxCharacter;
+
+	if (contador >=1) {
+		document.getElementById('boton').disabled=false;
+	}
+	if (contador ==0) {
+		document.getElementById('boton').disabled=true;
+	}
+})
+
 var boton = document.getElementById('boton');
-
 boton.addEventListener('click', function(){
-
 	//texto ingresado
 	var comentario = document.getElementById('twit').value;
 	//limpiar el textarea
@@ -18,3 +39,41 @@ boton.addEventListener('click', function(){
 
 	cont.appendChild(nuevoComentario);
 })
+
+
+/*
+var textarea = document.getElementsByTagName(textarea).value;
+textarea.addEventListener('onkeyup', function(){
+	//texto ingresado
+	if (textarea.value.length !== 0 || !== null ) {
+		document.getElementById('boton').disabled = false;
+	}
+	if (textarea.value.length === 0) {
+		document.getElementById('boton').disabled=true;
+	}
+})
+*/
+//evento de click para el boton
+
+
+//para que el boton inicie desactivado
+//document.getElementById('boton').disabled = true;
+
+
+
+
+/*textarea.addEventListener('keyup', function(){
+	var twitLength = textarea.length;
+		if (twitLength >=1){
+		document.getElementById('boton').disabled = false;
+	}
+		if(twitLength === 0){
+		document.getElementById('boton').disabled = true;
+	}
+})
+*/
+
+/*
+para deshabilitar boton
+document.getElementById("myBtn").disabled = true;
+*/
